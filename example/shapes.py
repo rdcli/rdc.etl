7cca2fab9fe697fe0e31be0ea6dcd43e29028bfb
Romain Dorgueil <romain@dorgueil.net>
2013-11-07 18:42:45 +0100

    better example output

--- example/shapes.py
+++ example/shapes.py
@@ -1,4 +1,4 @@
-from rdc.etl.status.console import ConsoleStatus
+import pprint
 from rdc.etl.transform.util import Log
 from rdc.etl.transform.extract import Extract
 from rdc.etl.harness.threaded2 import ThreadedHarness as ThreadedHarness2
@@ -7,17 +7,16 @@ from rdc.etl.harness.threaded import ThreadedHarness
 def build_producer(name):
     return Extract(({'producer': name, 'id': 1}, {'producer': name, 'id': 2}))
 
+print '>>> Test of simple linear shape'
+
 for Harness in ThreadedHarness, ThreadedHarness2:
-    print
-    print "-------------------------------"
     print "With %r" % Harness
-    print
-    print
     h = Harness()
-    h.status.append(ConsoleStatus())
     p1 = build_producer('p1')
     h.chain_add(p1, Log())
     h()
-    print
-    print
+
+    print 'Summary:'
+    pprint.pprint(h._transforms)
+    print '\n'
 
