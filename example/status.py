0bceb7e72101bc5948391c8db1884a27edaa5ae9
Romain Dorgueil <romain@dorgueil.net>
2013-11-30 16:21:26 +0100

    adds simplest example

---
+++ example/status.py
@@ -0,0 +1,19 @@
+from rdc.etl.contrib.example import Harness, build_producer, run
+from rdc.etl.transform.simple import SimpleTransform
+from rdc.etl.transform.util import Log
+
+print('########################')
+print('# Simplest ETL process #')
+print('########################')
+print
+print('Producer -> Transform -> Log')
+
+h = Harness()
+p = build_producer('Producer')
+t = SimpleTransform()
+h.add_chain(p, t, Log())
+run(h)
+
+print '\n'.join(map(repr, h.get_threads()))
+
+
