f608e5b49b11af82ebd50ba385fe8f8afe0442a9
Romain Dorgueil <romain@dorgueil.net>
2013-11-30 12:07:43 +0100

    Adds rdc.etl.transform.decorator:transformation decorator that will enhance any generator into a full power transformation. API not finalized, because there should be cool stuff to do to allow in/out channel customization with parameters for example.

---
+++ src/rdc/etl/transform/decorator.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2012-2013 Romain Dorgueil
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from rdc.etl.transform import Transform
+
+def transformation(method):
+    """Decorate a method so it becomes a simple transformation.
+
+    >>> @transformation
+    >>> def my_modifier(hash, channel):
+    ...     yield hash.copy((('foo', 'bar'), ))
+
+    """
+
+    t = Transform()
+    t.transform = method
+    t.__name__ = method.__name__
+    return t
+
+
+
